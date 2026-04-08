101405a03dd56dc54e0301c61d7b8865e4f175b6
Leonardo Prokopowiski <leoprokopowiskii@gmail.com>
2026-04-08 20:17:57 -0300

    feat: add function to visualize contacts

--- agenda.py
+++ agenda.py
@@ -3,6 +3,11 @@ def add_contact(contacts, contact_name, contact_phone, contact_email):
     contacts.append(contact)
     print(f"Contato '{contact_name}' adicionado com sucesso!")
 
+def visualize_contacts(contacts):
+    for index, contact in enumerate(contacts, start=1):
+        favorite = "★" if contact['favorite'] else " "
+        print(f"{index}. [{favorite}] {contact['name']} - {contact['phone']} - {contact['email']}")  
+
 contacts = []
 
 while True:
@@ -13,18 +18,21 @@ while True:
     print("4. Marcar/Desmarcar contato como favorito")
     print("5. Visualizar contatos favoritos")
     print("6. Apagar contato")
-    print("7. Visualizar detalhes de um contato")
-    print("8. Sair")
+    print("7. Sair")
 
     choice = input("\nDigite sua escolha: ")
 
     if choice == "1":
-        print("----- Adicionando Contato -----")
+        print("\n----- Adicionando Contato -----")
         contact_name = input("Digite o nome do seu contato: ")
         contact_phone = int(input("Digite o telefone: "))
         contact_email = input("Digite o email: ")
         add_contact(contacts, contact_name, contact_phone, contact_email)
-    elif choice == "8":
+    elif choice == "2":
+        print("\n----- Lista de Contatos -----")
+        visualize_contacts(contacts)
+    elif choice == "7":
+        print("Programa Encerrado!")
         break
     else:
         print("Opção inválida!")
