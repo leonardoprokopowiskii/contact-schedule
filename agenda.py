548d3f16e16d1673440e6b5ca4b58dd0270d64f5
Leonardo Prokopowiski <leoprokopowiskii@gmail.com>
2026-04-09 20:00:48 -0300

    feat: add function to mark or unmark a contact as a favorite

--- agenda.py
+++ agenda.py
@@ -3,16 +3,20 @@ def add_contact(contacts, contact_name, contact_phone, contact_email):
     contacts.append(contact)
     print(f"Contato '{contact_name}' adicionado com sucesso!")
 
+
 def visualize_contacts(contacts):
+    print("\n----- Lista de Contatos -----")
     for index, contact in enumerate(contacts, start=1):
         favorite = "★" if contact['favorite'] else " "
         print(f"{index}. [{favorite}] {contact['name']} - {contact['phone']} - {contact['email']}")
+
 
 def visualize_fields():
     print(f"\n----- Lista de Campos do Contato -----")
     print("1. Nome")
     print("2. Telefone")
     print("3. Email")
+
 
 def edit_contact(contacts, chosen_contact):
     try:
@@ -43,6 +47,25 @@ def edit_contact(contacts, chosen_contact):
     else:
         print("Opção inválida!")
 
+
+def mark_as_favorite_contact(contacts, chosen_contact):
+    try:
+        if int(chosen_contact) in range(1, (len(contacts) + 1)):
+            adjusted_chosen_contact = int(chosen_contact) - 1
+        else:
+            print("Não existe um contato registrado para o índice inserido!")
+            return
+    except Exception as e:
+        print(f"Erro: {e}")
+        return
+    
+    if contacts[adjusted_chosen_contact]['favorite'] == False:
+        contacts[adjusted_chosen_contact]['favorite'] = True
+        print(f"Contato '{contacts[adjusted_chosen_contact]['name']}' foi marcado como favorito!")
+    else:
+        contacts[adjusted_chosen_contact]['favorite'] = False
+        print(f"Contato '{contacts[adjusted_chosen_contact]['name']}' foi desmarcado como favorito!")
+
 contacts = []
 
 while True:
@@ -65,16 +88,21 @@ while True:
         add_contact(contacts, contact_name, contact_phone, contact_email)
     elif choice == "2":
         if len(contacts) > 0:
-            print("\n----- Lista de Contatos -----")
             visualize_contacts(contacts)
         else:
             print("Nenhum contato registrado!")
     elif choice == "3":
         if len(contacts) > 0:
-            print("\n----- Lista de Contatos -----")
             visualize_contacts(contacts)
             chosen_contact = input("\nDigite o número do contato que deseja editar: ")
             edit_contact(contacts, chosen_contact)
+        else:
+            print("Nenhum contato registrado!")
+    elif choice == "4":
+        if len(contacts) > 0:
+            visualize_contacts(contacts)
+            chosen_contact = input("\nDigite o número do contato que deseja marcar/desmarcar como favorito: ")
+            mark_as_favorite_contact(contacts, chosen_contact)
         else:
             print("Nenhum contato registrado!")
     elif choice == "7":
